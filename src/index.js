3a98d1e60cb3e583f04575423031030df2d966fa
Hai Ma <haima@Hais-MacBook-Pro.local>
2020-09-29 14:07:16 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {BrowserRouter as Router} from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
+  <Route>
     <App />
-  </React.StrictMode>,
+  </Route>,
   document.getElementById('root')
 );
 
