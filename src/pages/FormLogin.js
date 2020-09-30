0123d2d915adae121558dc335e6732c5ef5f1439
Hai Ma <haima@Hais-MacBook-Pro.local>
2020-09-30 13:38:07 +0700

    Dsa

--- src/pages/FormLogin.js
+++ src/pages/FormLogin.js
@@ -1,45 +1,60 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { useHistory } from "react-router-dom";
+import Form from 'react-bootstrap/Form'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 const FormLogin = () => {
     const history=useHistory();
     const submit=()=>{
         history.push(`/`)
     }
     return (
-        <div className="dien-form-login-parent">
+    
+        
+         <div className="dien-form-login-parent">
+         <Navbar bg="dark" variant="dark" fixed="top">
+            <div className="dien-parent">
+                    <img src="/images/logo-login.png" className="dien-image"/>
+            </div>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home"> </Nav.Link>
+                <Nav.Link href="#features"> </Nav.Link>
+                <Nav.Link href="#pricing"> </Nav.Link>
+            </Nav>
+        
+        </Navbar>
             <div className="dien-form-login">
-                <nav className="dien-nav-form-Login">
+                    <nav className="dien-nav-form-Login">
                     <div className="dien-nav-form-img">
                         <img src="/images/login-it.png" className="img-login-form"/>
                     </div>
                     <div className="dien-nav-form-img2">Login</div>            
                 </nav>
-                <div className="email-address">
-                    <h5 className="h5-email-address">Email address</h5>
-                </div> 
-                <div class="div-input-email">
-                    <input type="text" placeholder="Enter email" class="input-email"></input>
-                </div>
-                <div>
-                    <h4 className="h4-form-login">We'll never share your email with anyone else.</h4>
-                </div>
-                <div className="email-password">
-                    <h5 className="h5-email-password">Password</h5>
-                </div> 
-                <div class="div-input-password">
-                    <input type="text" placeholder="Password" class="input-password"></input>
-                </div>
-                <div className="login-form-button">
-                    <input type="checkbox" id="check"/>
-                    <label for="check">Check me out</label>
-                </div>
-                <div>
-                    <Button variant="danger" onClick={()=>submit()}>Submit</Button>
-                </div>
+                <Form>
+                <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
             </div>
-        </div>
-    )
+         </div>
+      
+        )
 }
 
 export default FormLogin
